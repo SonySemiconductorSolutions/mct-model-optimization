3336a07e8bf4efad0fd8d58ad1e396ee0fea9763
yt0705 <taguchi.yuta00@jp.panasonic.com>
2025-09-30 16:53:01 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(name='model_compression_toolkit',
       long_description=get_log_description(),
       long_description_content_type="text/markdown",
       description='A Model Compression Toolkit for neural networks',
-      author_email='ssi-dnn-dev@sony.com',
       packages=find_packages(include=["model_compression_toolkit", "model_compression_toolkit.*"]),
       classifiers=[
           "Programming Language :: Python :: 3",
@@ -51,5 +50,5 @@ setup(name='model_compression_toolkit',
           "Topic :: Scientific/Engineering :: Artificial Intelligence"
       ],
       install_requires=read_install_requires(),
-      python_requires='>=3.6'
+      python_requires='>=3.9'
       )
