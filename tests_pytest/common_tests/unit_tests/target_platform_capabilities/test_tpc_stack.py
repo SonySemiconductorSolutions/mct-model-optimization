c2e316ea649fd3976e8a3c25c597be6874078b7d
kawakami-masaki0 <kawakami.masaki@jp.panasonic.com>
2025-10-01 16:07:29 +0900

    Fix TPC for stack

---
+++ tests_pytest/common_tests/unit_tests/target_platform_capabilities/test_tpc_stack.py
@@ -0,0 +1,28 @@
+# Copyright 2025 Sony Semiconductor Solutions, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from model_compression_toolkit.target_platform_capabilities.tpc_models.imx500_tpc.v1.tpc import get_tpc as get_tpc_imx500_v1
+
+def test_tpc_stack():
+
+    tpc = get_tpc_imx500_v1() # only imx500 supported
+    opset = tpc.operator_set[0] # Stack Layer
+    assert opset.name == 'Stack'
+
+    qc = opset.qc_options.quantization_configurations[0]
+    assert qc.default_weight_attr_config.enable_weights_quantization == False
+    assert qc.attr_weights_configs_mapping == {}
+    assert qc.enable_activation_quantization == True
+    assert qc.activation_n_bits == 8
